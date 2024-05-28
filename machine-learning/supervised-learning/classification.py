7df3eb6c96889a7779040c19fadd90df797c45de
Subodh Uniyal <97154559+Subodh7976@users.noreply.github.com>
2024-05-28 17:55:50 +0000

    added naive bayes

--- machine-learning/supervised-learning/classification.py
+++ machine-learning/supervised-learning/classification.py
@@ -63,6 +63,51 @@ class KNN:
         return most_common[0][0]
 
 
+class NaiveBayes:
+    def __init__(self):
+        self.classes = None 
+        self.mean = None 
+        self.var = None 
+        self.priors = None 
+
+    def fit(self, X: np.array, y: np.array):
+        n_samples, n_features = X.shape 
+        self.classes = np.unique(y)
+        n_classes = len(self.classes)
+
+        self.mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self.classes):
+            X_c = X[y==c]
+            self.mean[idx, :] = X_c.mean(axis=0)
+            self.var[idx, :] = X_c.mean(axis=0)
+            self.priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X: np.array):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x: np.array):
+        posteriors = []
+
+        for idx, c in enumerate(self.classes):
+            prior = np.log(self.priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + posterior
+            posteriors.append(posterior)
+
+        return self.classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self.mean[class_idx]
+        var = self.var[class_idx]
+        numerator = np.exp(- (x - mean) ** 2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator 
+
+
 if __name__ == "__main__":
     X = np.random.rand(4, 2)
     y = np.array([1, 0, 0, 1])
@@ -84,3 +129,9 @@ if __name__ == "__main__":
     predicts = knn.predict(X)
     print("Predictions:\n", predicts)
     print("K:\n", knn.k)
+
+    print("\n", "-"*5, "Naive Bayes  ", "-"*5, "\n")
+    naive_bayes = NaiveBayes()
+    naive_bayes.fit(X, y)
+    predicts = naive_bayes.predict(X)
+    print("Predictions:\n", predicts)
