8e54e6e97e0edb4462e26d44994f3c274badb69b
Subodh Uniyal <97154559+Subodh7976@users.noreply.github.com>
2024-06-10 15:15:04 +0000

    added RMSProp

--- deep-learning/optimizers.py
+++ deep-learning/optimizers.py
@@ -83,3 +83,60 @@ class Adagrad(Optimizer):
 
         return loss 
     
+class RMSProp(Optimizer):
+    def __init__(self, params, lr=0.01, alpha=0.99, eps=1e-8, weight_decay=0, 
+                 momentum=0, centered=False):
+        defaults = dict(lr=lr, alpha=alpha, eps=eps, weight_decay=weight_decay,
+                        momentum=momentum, centered=centered)
+        super(RMSProp, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None 
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            lr = group['lr']
+            alpha = group['alpha']
+            eps = group['eps']
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            centered = group['centered']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data 
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['square_avg'] = torch.zeros_like(p.data)
+                    if momentum > 0:
+                        state['momentum_buffer'] = torch.zeros_like(p.data)
+                    if centered:
+                        state['grad_avg'] = torch.zeros_like(p.data)
+            
+                square_avg = state['square_avg']
+                alpha = group['alpha']
+                state['step'] += 1
+
+                square_avg.mul_(alpha).addcmul_(1 - alpha, grad, grad)
+                if centered:
+                    grad_avg = state['grad_avg']
+                    grad_avg.mul_(alpha).add_(1 - alpha, grad)
+                    avg = square_avg.addcmul_(-1, grad_avg, grad_avg).sqrt().add_(eps)
+                else:
+                    avg = square_avg.sqrt().add_(eps)
+                
+                if weight_decay != 0:
+                    grad = grad.add(weight_decay, p.data)
+                
+                if momentum > 0:
+                    buf = state['momentum_buffer']
+                    buf.mul_(momentum).addcdiv_(grad, avg)
+                    p.data.add_(-lr, buf)
+                else:
+                    p.data.addcdiv_(-lr, grad, avg)
+        
+        return loss 
