f96028bf59af826090c2054c12853c9241c6afbc
Subodh Uniyal <97154559+Subodh7976@users.noreply.github.com>
2024-06-14 06:14:16 +0000

    added linear layer

---
+++ deep-learning/neural_network/linear.py
@@ -0,0 +1,27 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias: bool = True):
+        super(Linear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+        
+    def forward(self, input: torch.tensor):
+        return F.linear(input, self.weight, self.bias)
